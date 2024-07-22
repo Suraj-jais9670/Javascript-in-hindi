5bb6d503b6a619bd2b595485406d1186fffa185d
Suraj Kumar Jaiswal <149613656+Suraj-jais9670@users.noreply.github.com>
2024-07-22 06:21:03 +0000

    datatype

---
+++ basic/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+//alert(3 + 3) // we are using nodejs, npt browser
+console.log(3
+    +3
+) // code readablity should be high
+
+console.log("suraj");
+
+let age =18
+let isLoggedIn = false
+let state;
+
+
+// number
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefind =>
+//symbol => unique 
+
+
+//object
+
+console.log(typeof null);
