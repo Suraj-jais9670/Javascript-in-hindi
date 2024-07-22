c737c895e9305fc4090c09ab254c208d20894249
Suraj Kumar Jaiswal <149613656+Suraj-jais9670@users.noreply.github.com>
2024-07-22 06:25:23 +0000

    variable

---
+++ basic/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 1243151
+let accountEmail = "surya@gamail.com"
+var accountPassward = "123456"
+accountCity = "LMP "
+let accountState;
+
+//accountId = 2// not allowed 
+
+/* prefer not to use var 
+because of issue in block scope and  functional scope */
+ 
+
+accountPassward = 456454554
+accountEmail = "suraj@gmail.com"
+accountCity = "lucknow"
+
+
+
+
+console.log(accountId);
+
+console.table([accountId , accountEmail , accountCity ,accountState])
