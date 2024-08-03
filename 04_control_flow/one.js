31be0ccb6e0bc9c3a2144df054ecd8ebed3528a8
Suraj Kumar Jaiswal <149613656+Suraj-jais9670@users.noreply.github.com>
2024-08-03 08:24:11 +0000

    control_flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,57 @@
+//if 
+
+// const isUserloggedIn =  true
+// const temprature = 41
+// if( temprature < 50){
+//     console.log("less than 50");
+    
+    
+// }else{
+//     console.log("greater than 50");
+    
+// }
+// console.log("execute");
+//<, > ,<= ,>=, ==, !=, ===, !==
+
+const score = 2000
+
+// if(score > 1000){
+//            const power = "fly"
+//            console.log(`User Power: ${power}`);
+           
+// }
+// console.log(`User Power: ${power}`);
+
+
+const balance = 10000
+
+// if(balance > 500) console.log("test"),
+// console.log("test2");
+
+
+// if(balance < 500){
+//     console.log("less then 500");
+    
+// }else if(balance < 750){
+//     console.log("less then 750");
+    
+
+// }
+// else if(balance > 900){
+//     console.log("less then 750");
+    
+// }
+// else{
+//     console.log("less then 900");
+    
+// }
+
+const UserloggedIn = true
+const debitCard = true
+
+if (UserloggedIn && debitCard){
+    console.log("allow to buy");
+    
+
+}
+
