be2facb915d2e544f7bfb98ea93a0c119c95fba5
Suraj Kumar Jaiswal <149613656+Suraj-jais9670@users.noreply.github.com>
2024-09-13 10:27:17 +0000

    five.js

---
+++ 05_iteration/five.js
@@ -0,0 +1,49 @@
+const coding =["js", "cpp","java", "ruby" ,"Css"]
+
+// coding .forEach( function(val) {
+//     console.log(val);
+    
+// })
+
+// coding.forEach((item) => {
+//     console.log(item);
+    
+// });
+
+// function printme (item){
+//     console.log(item);
+    
+// }
+// coding.forEach(printme)
+
+
+// coding.forEach((item, index, arr)=>{
+//     console.log(item, index ,arr);
+    
+// })
+
+
+const myCoding =[
+    {
+        languageName: 'javascript',
+        langaugefilename: 'JS'
+    },
+    {
+        languageName: 'Ruby',
+        langaugefilename: 'Ruby'
+    },
+    {
+        languageName: 'Java',
+        langaugefilename: 'Java'
+    },
+    {
+        languageName: 'Python',
+        langaugefilename: 'PY'
+    },
+  
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.langaugefilename);
+    
+})
---
+++ 05_iteration/four.js
@@ -0,0 +1,17 @@
+const myObject = {
+    js : 'javascipt' ,
+    cpp : 'c++', 
+    Swift : 'swift by Apple'
+}
+
+for (const key in myObject) {
+//   console.log(`${key} shortcut is for ${myObject[key]}`);
+  
+}
+
+const programming = ["js","ruby","CPP", "Swift"]
+
+for (const key in programming) {
+   console.log(programming[key]);
+   
+}
